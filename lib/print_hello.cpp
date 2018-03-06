018cb50d47194f17d03c81aacda8a3a22cef3642
wushifu32 <mailtowubo@163.com>
2018-03-06 12:31:18 +0800

    first commit

---
+++ lib/print_hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int print_hello(const char *msg)
+{
+    cout << "hello, " << msg << endl;
+    return 0;
+}
---
+++ lib/print_hello.h
@@ -0,0 +1,6 @@
+#ifndef __print_hello_h
+#define __print_hello_h
+
+int print_hello(const char *msg);
+
+#endif
